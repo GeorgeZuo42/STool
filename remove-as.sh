a8c5db2c46bf1dbaa30bc62f3cd7458f826e97b3
Enak <316165530@qq.com>
2017-07-28 12:24:04 +0800

    添加删除as

---
+++ remove-as.sh
@@ -0,0 +1,8 @@
+rm -rf /Applications/Android\ Studio.app
+rm -rf ~/Library/Preferences/AndroidStudio*
+rm -rf ~/Library/Application\ Support/AndroidStudio*
+rm -rf ~/Library/Logs/AndroidStudio*
+rm -rf ~/Library/Caches/AndroidStudio*
+rm -rf ~/.gradle
+rm -rf ~/.android
+rm -rf ~/Library/Android
